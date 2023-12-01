233bf95d8822b6559e3785b6de13d3c45234c319
YLMstring <61271096+YLMstring@users.noreply.github.com>
2023-12-01 16:42:02 +0800

    1

--- Blueprint/Feat/BodyGuard.cs
+++ Blueprint/Feat/BodyGuard.cs
@@ -1,6 +1,14 @@
-﻿using BlueprintCore.Blueprints.CustomConfigurators.Classes;
+﻿using BlueprintCore.Actions.Builder;
+using BlueprintCore.Blueprints.CustomConfigurators;
+using BlueprintCore.Blueprints.CustomConfigurators.Classes;
+using BlueprintCore.Blueprints.CustomConfigurators.Classes.Selection;
+using BlueprintCore.Blueprints.CustomConfigurators.UnitLogic.Abilities;
 using BlueprintCore.Blueprints.References;
 using BlueprintCore.Utils.Types;
+using Kingmaker.Blueprints.Classes;
+using Kingmaker.EntitySystem.Stats;
+using Kingmaker.Enums;
+using Kingmaker.UnitLogic.Abilities.Components.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -50,6 +58,79 @@ namespace PrestigePlus.Blueprint.Feat
                     .AddFacts(new() { FeatureRefs.MythicIgnoreAlignmentRestrictions.ToString(), AbilityRefs.DivineGuardianTrothAbility.ToString(), ActivatableAbilityRefs.DivineGuardianInHarmsWayAbility.ToString() })
                     .AddToGroups(Kingmaker.Blueprints.Classes.FeatureGroup.CombatFeat)
                     .Configure();
+        }
+
+        private const string NinjaTrickFeat = "NinjaTrick.NinjaTrick";
+        public static readonly string NinjaTrickGuid = "{4C242C11-9754-47BF-BD40-5C10489999A3}";
+
+        internal const string NinjaTrickDisplayName = "NinjaTrick.Name";
+        private const string NinjaTrickDescription = "NinjaTrick.Description";
+        public static void NinjaTrickConfigure()
+        {
+            var icon = AbilityRefs.TrickeryBlessingMajorAbility.Reference.Get().Icon;
+
+            var feat = FeatureSelectionConfigurator.New(NinjaTrickFeat, NinjaTrickGuid)
+              .SetDisplayName(NinjaTrickDisplayName)
+              .SetDescription(NinjaTrickDescription)
+              .SetIcon(icon)
+              .SetIgnorePrerequisites(false)
+              .SetObligatory(false)
+              .AddToAllFeatures(UnarmedCombatFeat())
+              .AddToAllFeatures(ArrowSnatchFeat())
+              .Configure();
+
+            FeatureSelectionConfigurator.For(FeatureSelectionRefs.RogueTalentSelection)
+                .AddToAllFeatures(feat)
+                .Configure();
+
+            FeatureSelectionConfigurator.For(FeatureSelectionRefs.DarkLurkerRogueTalentSelection)
+                .AddToAllFeatures(feat)
+                .Configure();
+
+            FeatureSelectionConfigurator.For(FeatureSelectionRefs.ExtraRogueTalentSelection)
+                .AddToAllFeatures(feat)
+                .Configure();
+
+            FeatureSelectionConfigurator.For(FeatureSelectionRefs.LoremasterRogueTalentSelection)
+                .AddToAllFeatures(feat)
+                .Configure();
+        }
+
+        private const string UnarmedCombat = "NinjaTrick.UnarmedCombat";
+        public static readonly string UnarmedCombatGuid = "{5BFCCF83-947B-4F32-8BFC-C304A8591594}";
+
+        internal const string UnarmedCombatDisplayName = "NinjaTrickUnarmedCombat.Name";
+        private const string UnarmedCombatDescription = "NinjaTrickUnarmedCombat.Description";
+
+        public static BlueprintFeature UnarmedCombatFeat()
+        {
+            var icon = FeatureRefs.ImprovedUnarmedStrike.Reference.Get().Icon;
+
+            return FeatureConfigurator.New(UnarmedCombat, UnarmedCombatGuid)
+              .SetDisplayName(UnarmedCombatDisplayName)
+              .SetDescription(UnarmedCombatDescription)
+              .SetIcon(icon)
+              .AddFacts(new() { FeatureRefs.ImprovedUnarmedStrike.ToString() })
+              .Configure();
+        }
+
+        private const string ArrowSnatch = "NinjaTrick.ArrowSnatch";
+        public static readonly string ArrowSnatchGuid = "{222D49BE-3885-41BF-8A34-EB2ED46950FB}";
+
+        internal const string ArrowSnatchDisplayName = "NinjaTrickArrowSnatch.Name";
+        private const string ArrowSnatchDescription = "NinjaTrickArrowSnatch.Description";
+
+        public static BlueprintFeature ArrowSnatchFeat()
+        {
+            var icon = FeatureRefs.DeflectArrows.Reference.Get().Icon;
+
+            return FeatureConfigurator.New(ArrowSnatch, ArrowSnatchGuid)
+              .SetDisplayName(ArrowSnatchDisplayName)
+              .SetDescription(ArrowSnatchDescription)
+              .SetIcon(icon)
+              .AddPrerequisiteFeature(FeatureRefs.ImprovedUnarmedStrike.ToString())
+              .AddFacts(new() { FeatureRefs.DeflectArrows.ToString() })
+              .Configure();
         }
     }
 }
--- Main.cs
+++ Main.cs
@@ -300,7 +300,7 @@ namespace PrestigePlus
                     if (ModMenu.ModMenu.GetSettingValue<bool>(GetKey("warlord"))) { Warlord.Configure(); }
                     if (ModMenu.ModMenu.GetSettingValue<bool>(GetKey("ragonury"))) { DragonFury.Configure(); }
                     if (ModMenu.ModMenu.GetSettingValue<bool>(GetKey("dawnflower"))) { AnchoriteofDawn.Configure(); }
-                    if (ModMenu.ModMenu.GetSettingValue<bool>(GetKey("bodyguard"))) { BodyGuard.Configure(); BodyGuard.Configure2(); }
+                    if (ModMenu.ModMenu.GetSettingValue<bool>(GetKey("bodyguard"))) { BodyGuard.Configure(); BodyGuard.Configure2(); BodyGuard.NinjaTrickConfigure(); }
                     if (ModMenu.ModMenu.GetSettingValue<bool>(GetKey("atheism"))) { DivineDefiance.StyleConfigure(); DivineDefiance.IconoclastConfigure(); }
                     if (ModMenu.ModMenu.GetSettingValue<bool>(GetKey("rogue"))) { QuickShot.Configure(); CloyingShades.CloyingShadesFeat(); DefensiveRoll.Configure(); SneakyManeuver.SneakyManeuverFeat(); SurpriseManeuver.Configure(); }
                     if (ModMenu.ModMenu.GetSettingValue<bool>(GetKey("kineticknight"))) { EsotericKnight.Configure(); }
