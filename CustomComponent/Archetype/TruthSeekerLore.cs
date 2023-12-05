f1b7ac5e818ce775deed3e713b14ebf431a7a00f
YLMstring <61271096+YLMstring@users.noreply.github.com>
2023-12-05 13:06:15 +0800

    1

--- CustomComponent/Archetype/TruthSeekerLore.cs
+++ CustomComponent/Archetype/TruthSeekerLore.cs
@@ -1,8 +1,12 @@
 ﻿using BlueprintCore.Blueprints.References;
+using Kingmaker.Blueprints;
 using Kingmaker.Blueprints.Classes.Spells;
+using Kingmaker.Designers.Mechanics.Facts;
 using Kingmaker.PubSubSystem;
 using Kingmaker.RuleSystem.Rules.Abilities;
 using Kingmaker.UnitLogic;
+using Kingmaker.UnitLogic.Abilities.Blueprints;
+using Kingmaker.UnitLogic.FactLogic;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,9 +20,9 @@ namespace PrestigePlus.CustomComponent.Archetype
         void IRulebookHandler<RuleCalculateAbilityParams>.OnEventAboutToTrigger(RuleCalculateAbilityParams evt)
         {
             var spellbook = Owner.GetSpellbook(CharacterClassRefs.OracleClass.Reference);
-            var spelllist = SpellListRefs.ClericSpellList.Reference.Get();
+            //var spelllist = SpellListRefs.ClericSpellList.Reference.Get();
             var spell = evt.AbilityData?.ConvertedFrom?.Blueprint ?? evt.Spell;
-            if (spellbook != null && evt.Spellbook == spellbook && !spelllist.Contains(spell))
+            if (spellbook != null && evt.Spellbook == spellbook && SpellList.Contains(spell))
             {
                 evt.AddBonusCasterLevel(4, Kingmaker.Enums.ModifierDescriptor.UntypedStackable);
             }
@@ -28,5 +32,34 @@ namespace PrestigePlus.CustomComponent.Archetype
         {
             
         }
+
+        public override void OnActivate()
+        {
+            var list = FeatureSelectionRefs.OracleMysterySelection.Reference.Get().m_AllFeatures;
+            foreach ( var mystery in list )
+            {
+                if (!Owner.HasFact(mystery)) { continue; }
+                var comps = mystery.Get().GetComponents<AddFeatureOnClassLevel>();
+                if (comps?.Any() != true) { continue; }
+                foreach (var item in comps)
+                {
+                    if (item.Level != 2) { continue; }
+                    var spells = item.m_Feature?.Get().GetComponents<AddKnownSpell>();
+                    if (spells?.Any() != true) { continue; }
+                    foreach (var spell in spells)
+                    {
+                        if (spell.m_Spell == null) { continue; }
+                        SpellList.Add(spell.m_Spell);
+                    }
+                }
+            }
+        }
+
+        public override void OnDeactivate()
+        {
+            SpellList = new() { };
+        }
+
+        private List<BlueprintAbility> SpellList = new() { };
     }
 }
--- CustomComponent/Archetype/TruthSeekerMagic.cs
+++ CustomComponent/Archetype/TruthSeekerMagic.cs
@@ -1,7 +1,11 @@
 ﻿using BlueprintCore.Blueprints.References;
+using Kingmaker.Blueprints;
+using Kingmaker.Designers.Mechanics.Facts;
 using Kingmaker.PubSubSystem;
 using Kingmaker.RuleSystem.Rules;
 using Kingmaker.UnitLogic;
+using Kingmaker.UnitLogic.Abilities.Blueprints;
+using Kingmaker.UnitLogic.FactLogic;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,8 +25,8 @@ namespace PrestigePlus.CustomComponent.Archetype
         void IRulebookHandler<RuleApplyMetamagic>.OnEventDidTrigger(RuleApplyMetamagic evt)
         {
             var spellbook = Owner.GetSpellbook(CharacterClassRefs.OracleClass.Reference);
-            var spelllist = SpellListRefs.ClericSpellList.Reference.Get();
-            if (spellbook != null && evt.Spellbook == spellbook && !spelllist.Contains(evt.Spell))
+            //var spelllist = SpellListRefs.ClericSpellList.Reference.Get();
+            if (spellbook != null && evt.Spellbook == spellbook && SpellList.Contains(evt.Spell))
             {
                 evt.Result.SpellLevelCost -= evt.AppliedMetamagics.Count;
                 if (evt.BaseLevel + evt.Result.SpellLevelCost < 0)
@@ -31,5 +35,34 @@ namespace PrestigePlus.CustomComponent.Archetype
                 }
             }
         }
+
+        public override void OnActivate()
+        {
+            var list = FeatureSelectionRefs.OracleMysterySelection.Reference.Get().m_AllFeatures;
+            foreach (var mystery in list)
+            {
+                if (!Owner.HasFact(mystery)) { continue; }
+                var comps = mystery.Get().GetComponents<AddFeatureOnClassLevel>();
+                if (comps?.Any() != true) { continue; }
+                foreach (var item in comps)
+                {
+                    if (item.Level != 2) { continue; }
+                    var spells = item.m_Feature?.Get().GetComponents<AddKnownSpell>();
+                    if (spells?.Any() != true) { continue; }
+                    foreach (var spell in spells)
+                    {
+                        if (spell.m_Spell == null) { continue; }
+                        SpellList.Add(spell.m_Spell);
+                    }
+                }
+            }
+        }
+
+        public override void OnDeactivate()
+        {
+            SpellList = new() { };
+        }
+
+        private List<BlueprintAbility> SpellList = new() { };
     }
 }
