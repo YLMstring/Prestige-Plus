5785de4f9e98aa45a871e367171d3b1e2cfb4732
YLMstring <61271096+YLMstring@users.noreply.github.com>
2024-01-17 13:32:51 +0800

    1

---
+++ Blueprint/Archetype/FatedChampion.cs
@@ -0,0 +1,130 @@
+﻿using BlueprintCore.Actions.Builder;
+using BlueprintCore.Blueprints.CustomConfigurators.Classes.Selection;
+using BlueprintCore.Blueprints.CustomConfigurators.Classes;
+using BlueprintCore.Blueprints.CustomConfigurators.UnitLogic.Abilities;
+using BlueprintCore.Blueprints.CustomConfigurators.UnitLogic.Buffs;
+using BlueprintCore.Blueprints.References;
+using BlueprintCore.Utils.Types;
+using Kingmaker.Blueprints.Classes;
+using Kingmaker.Blueprints.Items.Armors;
+using Kingmaker.Enums;
+using Kingmaker.UnitLogic.Abilities.Blueprints;
+using Kingmaker.UnitLogic.Abilities;
+using PrestigePlus.CustomAction.ClassRelated;
+using PrestigePlus.CustomComponent.Archetype;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kingmaker.EntitySystem.Stats;
+using Kingmaker.Blueprints.Classes.Spells;
+
+namespace PrestigePlus.Blueprint.Archetype
+{
+    internal class FatedChampion
+    {
+        private const string ArchetypeName = "FatedChampion";
+        private static readonly string ArchetypeGuid = "{6848CEF0-701C-45BF-A43E-A58E403579DD}";
+        internal const string ArchetypeDisplayName = "FatedChampion.Name";
+        private const string ArchetypeDescription = "FatedChampion.Description";
+        public static void Configure()
+        {
+            ArchetypeConfigurator.New(ArchetypeName, ArchetypeGuid, CharacterClassRefs.SkaldClass)
+              .SetLocalizedName(ArchetypeDisplayName)
+              .SetLocalizedDescription(ArchetypeDescription)
+            .SetRemoveFeaturesEntry(2, FeatureRefs.SkaldWellVersed.ToString())
+            .SetRemoveFeaturesEntry(10, FeatureRefs.DirgeOfDoom.ToString())
+            .SetRemoveFeaturesEntry(20, FeatureRefs.MasterSkald.ToString())
+            .AddToAddFeatures(2, CreateWatcher())
+            .AddToAddFeatures(3, CreateShieldForesight())
+            .AddToAddFeatures(4, CreateNotThisDay())
+              .Configure();
+        }
+        //2 10 20
+        private const string ShieldForesight = "FatedChampion.ShieldForesight";
+        public static readonly string ShieldForesightGuid = "{09C3C05C-D642-4A43-97A3-4B0F065D235C}";
+
+        private const string ShieldForesightFeat = "FatedChampion.UseShieldForeFeat";
+        private static readonly string ShieldForesightFeatGuid = "{3E41D736-B7E9-4978-8FFE-5EF37034DFCE}";
+
+        private const string ShieldForesightBuff = "FatedChampion.ShieldForesightBuff";
+        private static readonly string ShieldForesightBuffGuid = "{22F69E4B-DFCF-4A35-B16E-AD588A3A6CB8}";
+
+        internal const string ShieldForesightDisplayName = "FatedChampionShieldForesight.Name";
+        private const string ShieldForesightDescription = "FatedChampionShieldForesight.Description";
+
+        public static BlueprintFeature CreateShieldForesight()
+        {
+            var icon = AbilityRefs.Foresight.Reference.Get().Icon;
+
+            var feat = FeatureConfigurator.New(ShieldForesightFeat, ShieldForesightFeatGuid)
+              .SetDisplayName(ShieldForesightDisplayName)
+              .SetDescription(ShieldForesightDescription)
+              .SetIcon(icon)
+              .SetIsClassFeature(true)
+              .AddSavingThrowBonusAgainstDescriptor(spellDescriptor: SpellDescriptor.Fear, value: 5)
+              .Configure();
+
+            var Buff = BuffConfigurator.New(ShieldForesightBuff, ShieldForesightBuffGuid)
+             .SetDisplayName(ShieldForesightDisplayName)
+             .SetDescription(ShieldForesightDescription)
+             .SetIcon(icon)
+             .AddFacts(new() { feat })
+             .AddBuffDescriptorImmunity(false, SpellDescriptor.Fear)
+             .AddSpellImmunityToSpellDescriptor(descriptor: SpellDescriptor.Fear)
+             .Configure();
+
+            BuffConfigurator.For(BuffRefs.InspiredRageEffectBuff)
+                .AddFactsFromCaster(new() { feat }, false)
+                .Configure();
+
+            return FeatureConfigurator.New(ShieldForesight, ShieldForesightGuid)
+              .SetDisplayName(ShieldForesightDisplayName)
+              .SetDescription(ShieldForesightDescription)
+              .SetIcon(icon)
+              .SetIsClassFeature(true)
+              .AddBuffExtraEffects(BuffRefs.InspiredRageBuff.ToString(), extraEffectBuff: Buff)
+              .Configure();
+        }
+
+        private const string Watcher = "FatedChampion.Watcher";
+        private static readonly string WatcherGuid = "{2D8C398F-84E9-492F-854E-B99154755692}";
+
+        internal const string WatcherDisplayName = "FatedChampionWatcher.Name";
+        private const string WatcherDescription = "FatedChampionWatcher.Description";
+
+        private static BlueprintFeature CreateWatcher()
+        {
+            var icon = AbilityRefs.SeeInvisibility.Reference.Get().Icon;
+
+            return FeatureConfigurator.New(Watcher, WatcherGuid)
+              .SetDisplayName(WatcherDisplayName)
+              .SetDescription(WatcherDescription)
+              .SetIcon(icon)
+              .AddContextStatBonus(StatType.Initiative, ContextValues.Rank(), ModifierDescriptor.Insight)
+              .AddContextRankConfig(ContextRankConfigs.ClassLevel(new string[] { CharacterClassRefs.SkaldClass.ToString() }).WithDiv2Progression())
+              .SetReapplyOnLevelUp(true)
+              .Configure();
+        }
+
+        private const string NotThisDay = "FatedChampion.NotThisDay";
+        public static readonly string NotThisDayGuid = "{42886C52-AB8C-47E3-B253-2F1A45D1423A}";
+
+        internal const string NotThisDayDisplayName = "FatedChampionNotThisDay.Name";
+        private const string NotThisDayDescription = "FatedChampionNotThisDay.Description";
+
+        private static readonly string DefensiveGuid = "3967B46C-99CA-41E9-B7A2-96644EED8C96";
+        public static BlueprintFeature CreateNotThisDay()
+        {
+            var icon = FeatureRefs.SlipperyMind.Reference.Get().Icon;
+
+            return FeatureConfigurator.New(NotThisDay, NotThisDayGuid)
+              .SetDisplayName(NotThisDayDisplayName)
+              .SetDescription(NotThisDayDescription)
+              .SetIcon(icon)
+              .AddFacts(new() { FeatureRefs.SlipperyMind.ToString(), DefensiveGuid })
+              .Configure();
+        }
+    }
+}
---
+++ Blueprint/Archetype/Toxicologist.cs
@@ -0,0 +1,77 @@
+﻿using BlueprintCore.Blueprints.CustomConfigurators.Classes;
+using BlueprintCore.Blueprints.References;
+using BlueprintCore.Utils.Types;
+using Kingmaker.Blueprints.Classes;
+using Kingmaker.Blueprints.Classes.Spells;
+using Kingmaker.EntitySystem.Stats;
+using Kingmaker.Enums;
+using Kingmaker.UnitLogic.FactLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrestigePlus.Blueprint.Archetype
+{
+    internal class Toxicologist
+    {
+        private const string ArchetypeName = "Toxicologist";
+        private static readonly string ArchetypeGuid = "{8C90A5EC-3CB9-4C75-832B-B468091E8544}";
+        internal const string ArchetypeDisplayName = "Toxicologist.Name";
+        private const string ArchetypeDescription = "Toxicologist.Description";
+        public static void Configure()
+        {
+            ArchetypeConfigurator.New(ArchetypeName, ArchetypeGuid, CharacterClassRefs.DruidClass)
+              .SetLocalizedName(ArchetypeDisplayName)
+              .SetLocalizedDescription(ArchetypeDescription)
+            .SetRemoveFeaturesEntry(2, FeatureRefs.DruidWoodlandStride.ToString())
+            .SetRemoveFeaturesEntry(4, FeatureRefs.ResistNaturesLure.ToString())
+            .SetRemoveFeaturesEntry(6, FeatureRefs.WildShapeElementalSmallFeature.ToString())
+            .SetRemoveFeaturesEntry(8, FeatureRefs.WildShapeElementalFeatureAddMedium.ToString())
+            .SetRemoveFeaturesEntry(10, FeatureRefs.WildShapeElementalFeatureAddLarge.ToString(), FeatureRefs.WildShapeIVShamblingMoundFeature.ToString())
+            .SetRemoveFeaturesEntry(12, FeatureRefs.WildShapeElementaHugeFeature.ToString())
+            .AddToAddFeatures(1, CreateExpandedRepertoire())
+            .AddToAddFeatures(4, ExpertPoisonerConfigure())
+              .Configure();
+
+        }
+
+        private const string ExpandedRepertoire = "Toxicologist.ExpandedRepertoire";
+        public static readonly string ExpandedRepertoireGuid = "{B2F76FAE-B259-4953-9D4B-2131524E24CE}";
+
+        internal const string ExpandedRepertoireDisplayName = "ToxicologistExpandedRepertoire.Name";
+        private const string ExpandedRepertoireDescription = "ToxicologistExpandedRepertoire.Description";
+
+        public static BlueprintFeature CreateExpandedRepertoire()
+        {
+            var icon = AbilityRefs.StinkingCloud.Reference.Get().Icon;
+
+            return FeatureConfigurator.New(ExpandedRepertoire, ExpandedRepertoireGuid)
+              .SetDisplayName(ExpandedRepertoireDisplayName)
+              .SetDescription(ExpandedRepertoireDescription)
+              .SetIcon(icon)
+              .AddKnownSpell(characterClass: CharacterClassRefs.DruidClass.ToString(), spellLevel: 3, spell: AbilityRefs.StinkingCloud.ToString())
+              .AddKnownSpell(characterClass: CharacterClassRefs.DruidClass.ToString(), spellLevel: 5, spell: AbilityRefs.Cloudkill.ToString())
+              .Configure();
+        }
+
+        private const string ExpertPoisoner = "Toxicologist.ExpertPoisoner";
+        public static readonly string ExpertPoisonerGuid = "{6625455D-05A4-4A2A-835B-18C806467081}";
+
+        internal const string ExpertPoisonerDisplayName = "ToxicologistExpertPoisoner.Name";
+        private const string ExpertPoisonerDescription = "ToxicologistExpertPoisoner.Description";
+
+        public static BlueprintFeature ExpertPoisonerConfigure()
+        {
+            var icon = AbilityRefs.Poison.Reference.Get().Icon;
+
+            return FeatureConfigurator.New(ExpertPoisoner, ExpertPoisonerGuid)
+              .SetDisplayName(ExpertPoisonerDisplayName)
+              .SetDescription(ExpertPoisonerDescription)
+              .SetIcon(icon)
+              .AddIncreaseSpellDescriptorDC(1, SpellDescriptor.Poison, spellsOnly: true)
+              .Configure();
+        }
+    }
+}
