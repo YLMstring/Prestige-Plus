b39733cb5fc00ae584a22a247f4a167e5a96811a
YLMstring <61271096+YLMstring@users.noreply.github.com>
2024-01-24 15:14:15 +0800

    1

--- Blueprint/Feat/Inquisition.cs
+++ Blueprint/Feat/Inquisition.cs
@@ -22,6 +22,12 @@ using Kingmaker.UnitLogic.Mechanics.Conditions;
 using BlueprintCore.Actions.Builder.ContextEx;
 using BlueprintCore.Utils.Types;
 using Kingmaker.UnitLogic.Mechanics;
+using BlueprintCore.Blueprints.Configurators.UnitLogic.ActivatableAbilities;
+using BlueprintCore.Blueprints.CustomConfigurators;
+using Kingmaker.UnitLogic.ActivatableAbilities;
+using Kingmaker.UnitLogic.Buffs.Blueprints;
+using Kingmaker.UnitLogic.Mechanics.Properties;
+using Kingmaker.Designers.Mechanics.Facts;
 
 namespace PrestigePlus.Blueprint.Feat
 {
@@ -64,27 +70,27 @@ namespace PrestigePlus.Blueprint.Feat
               .SetIcon(icon)
               .SetIgnorePrerequisites(false)
               .SetObligatory(true)
-              .AddToAllFeatures(VitalityGoodFeat())
-              .AddToAllFeatures(VitalityEvilFeat())
+              .AddToAllFeatures(HunterGoodFeat())
+              .AddToAllFeatures(HunterEvilFeat())
               .AddFacts(new() { FeatureRefs.Disruptive.ToString() })
               .Configure();
         }
 
-        private const string VitalityGood = "Inquisition.VitalityGood";
-        private static readonly string VitalityGoodGuid = "{089CB193-5638-4ADB-9B18-35023FD8ABA7}";
+        private const string HunterGood = "Inquisition.HunterGood";
+        private static readonly string HunterGoodGuid = "{089CB193-5638-4ADB-9B18-35023FD8ABA7}";
 
-        private const string VitalityGoodBuff = "Inquisition.VitalityGoodBuff";
-        private static readonly string VitalityGoodBuffGuid = "{C1B2FB40-8EE4-455B-AEF1-AD58A4EBF98B}";
+        private const string HunterGoodBuff = "Inquisition.HunterGoodBuff";
+        private static readonly string HunterGoodBuffGuid = "{C1B2FB40-8EE4-455B-AEF1-AD58A4EBF98B}";
 
-        internal const string InquisitionVitalityGoodDisplayName = "InquisitionVitalityGood.Name";
-        private const string InquisitionVitalityGoodDescription = "InquisitionVitalityGood.Description";
-        public static BlueprintFeature VitalityGoodFeat()
+        internal const string InquisitionHunterGoodDisplayName = "InquisitionHunterGood.Name";
+        private const string InquisitionHunterGoodDescription = "InquisitionHunterGood.Description";
+        public static BlueprintFeature HunterGoodFeat()
         {
             var icon = AbilityRefs.ShieldOfDawn.Reference.Get().Icon;
 
-            var Buff1 = BuffConfigurator.New(VitalityGoodBuff, VitalityGoodBuffGuid)
-              .SetDisplayName(InquisitionVitalityGoodDisplayName)
-              .SetDescription(InquisitionVitalityGoodDescription)
+            var Buff1 = BuffConfigurator.New(HunterGoodBuff, HunterGoodBuffGuid)
+              .SetDisplayName(InquisitionHunterGoodDisplayName)
+              .SetDescription(InquisitionHunterGoodDescription)
               .SetIcon(icon)
               .AddStatBonus(ModifierDescriptor.Sacred, false, StatType.AC, 2)
               .Configure();
@@ -93,9 +99,9 @@ namespace PrestigePlus.Blueprint.Feat
                 .ApplyBuff(Buff1, ContextDuration.Variable(ContextValues.Rank(), DurationRate.Minutes), toCaster: true)
                 .Build();
 
-            return FeatureConfigurator.New(VitalityGood, VitalityGoodGuid)
-              .SetDisplayName(InquisitionVitalityGoodDisplayName)
-              .SetDescription(InquisitionVitalityGoodDescription)
+            return FeatureConfigurator.New(HunterGood, HunterGoodGuid)
+              .SetDisplayName(InquisitionHunterGoodDisplayName)
+              .SetDescription(InquisitionHunterGoodDescription)
               .SetIcon(icon)
               .AddComponent<AddOutgoingDamageTriggerTTT>(c => { c.Actions = action; c.TargetKilledByThisDamage = true; })
               .AddContextRankConfig(ContextRankConfigs.ClassLevel(new string[] { CharacterClassRefs.InquisitorClass.ToString(), CharacterClassRefs.ClericClass.ToString() }, min: 1))
@@ -104,21 +110,21 @@ namespace PrestigePlus.Blueprint.Feat
               .Configure();
         }
 
-        private const string VitalityEvil = "Inquisition.VitalityEvil";
-        private static readonly string VitalityEvilGuid = "{97CE4717-8D06-4C91-AAC3-B88A6035DC8A}";
+        private const string HunterEvil = "Inquisition.HunterEvil";
+        private static readonly string HunterEvilGuid = "{97CE4717-8D06-4C91-AAC3-B88A6035DC8A}";
 
-        private const string VitalityEvilBuff = "Inquisition.VitalityEvilBuff";
-        private static readonly string VitalityEvilBuffGuid = "{06AFFB6A-946C-42EA-BE41-87BA13B7C1A2}";
+        private const string HunterEvilBuff = "Inquisition.HunterEvilBuff";
+        private static readonly string HunterEvilBuffGuid = "{06AFFB6A-946C-42EA-BE41-87BA13B7C1A2}";
 
-        internal const string InquisitionVitalityEvilDisplayName = "InquisitionVitalityEvil.Name";
-        private const string InquisitionVitalityEvilDescription = "InquisitionVitalityEvil.Description";
-        public static BlueprintFeature VitalityEvilFeat()
+        internal const string InquisitionHunterEvilDisplayName = "InquisitionHunterEvil.Name";
+        private const string InquisitionHunterEvilDescription = "InquisitionHunterEvil.Description";
+        public static BlueprintFeature HunterEvilFeat()
         {
             var icon = AbilityRefs.VampiricShadowShield.Reference.Get().Icon;
 
-            var Buff1 = BuffConfigurator.New(VitalityEvilBuff, VitalityEvilBuffGuid)
-              .SetDisplayName(InquisitionVitalityEvilDisplayName)
-              .SetDescription(InquisitionVitalityEvilDescription)
+            var Buff1 = BuffConfigurator.New(HunterEvilBuff, HunterEvilBuffGuid)
+              .SetDisplayName(InquisitionHunterEvilDisplayName)
+              .SetDescription(InquisitionHunterEvilDescription)
               .SetIcon(icon)
               .AddStatBonus(ModifierDescriptor.Profane, false, StatType.AC, 2)
               .Configure();
@@ -127,14 +133,105 @@ namespace PrestigePlus.Blueprint.Feat
                 .ApplyBuff(Buff1, ContextDuration.Variable(ContextValues.Rank(), DurationRate.Minutes), toCaster: true)
                 .Build();
 
-            return FeatureConfigurator.New(VitalityEvil, VitalityEvilGuid)
-              .SetDisplayName(InquisitionVitalityEvilDisplayName)
-              .SetDescription(InquisitionVitalityEvilDescription)
+            return FeatureConfigurator.New(HunterEvil, HunterEvilGuid)
+              .SetDisplayName(InquisitionHunterEvilDisplayName)
+              .SetDescription(InquisitionHunterEvilDescription)
               .SetIcon(icon)
               .AddComponent<AddOutgoingDamageTriggerTTT>(c => { c.Actions = action; c.TargetKilledByThisDamage = true; })
               .AddContextRankConfig(ContextRankConfigs.ClassLevel(new string[] { CharacterClassRefs.InquisitorClass.ToString(), CharacterClassRefs.ClericClass.ToString() }, min: 1))
               .AddPrerequisiteNoFeature(FeatureRefs.GoodDomainAllowed.ToString())
               .SetHideInCharacterSheetAndLevelUp()
+              .Configure();
+        }
+
+        private const string Reformation = "Inquisition.Reformation";
+        private static readonly string ReformationGuid = "{E0306AAC-6929-4459-811B-D2AD70878979}";
+
+        internal const string ReformationDisplayName = "InquisitionReformation.Name";
+        private const string ReformationDescription = "InquisitionReformation.Description";
+        public static BlueprintProgression ReformationFeat()
+        {
+            var icon = FeatureRefs.FortuneRevelationFeature.Reference.Get().Icon;
+
+            return ProgressionConfigurator.New(Reformation, ReformationGuid)
+              .SetDisplayName(ReformationDisplayName)
+              .SetDescription(ReformationDescription)
+              .SetIcon(icon)
+              .SetIsClassFeature(true)
+              .SetClasses(ProgressionRefs.AirDomainProgression.Reference.Get().m_Classes)
+              .SetArchetypes(ProgressionRefs.AirDomainProgression.Reference.Get().m_Archetypes)
+              .SetGiveFeaturesForPreviousLevels(true)
+              .AddToGroups(FeatureGroup.Domain)
+              .AddToLevelEntry(1, InspiredRhetoricFeat())
+              .AddToLevelEntry(4, BlessedCorrectionFeat())
+              .Configure();
+        }
+
+        private const string BlessedCorrection = "Inquisition.BlessedCorrection";
+        public static readonly string BlessedCorrectionGuid = "{39E540A1-EAB7-4B8D-BD81-851D089C4BDA}";
+
+        internal const string BlessedCorrectionDisplayName = "InquisitionBlessedCorrection.Name";
+        private const string BlessedCorrectionDescription = "InquisitionBlessedCorrection.Description";
+
+        private const string BlessedCorrectionBuff = "Inquisition.BlessedCorrectionBuff";
+        private static readonly string BlessedCorrectionBuffGuid = "{AE2ECC69-5F8E-42DB-800C-E4DD7D9DDFBF}";
+
+        private const string BlessedCorrectionRes = "Inquisition.BlessedCorrectionRes";
+        private static readonly string BlessedCorrectionResGuid = "{8BF4D53D-A59F-4C66-903E-7CC852A47F22}";
+
+        private const string BlessedCorrectionAbility = "Inquisition.BlessedCorrectionAbility";
+        private static readonly string BlessedCorrectionAbilityGuid = "{D463B68A-9CD4-4196-8A6F-B0053FB5FEA4}";
+        public static BlueprintFeature BlessedCorrectionFeat()
+        {
+            var icon = FeatureRefs.Deceitful.Reference.Get().Icon;
+
+            var abilityresourse = AbilityResourceConfigurator.New(BlessedCorrectionRes, BlessedCorrectionResGuid)
+                .SetMaxAmount(ResourceAmountBuilder.New(0)
+                    .IncreaseByStat(StatType.Wisdom))
+                .Configure();
+
+            var ability = ActivatableAbilityConfigurator.New(BlessedCorrectionAbility, BlessedCorrectionAbilityGuid)
+                .SetDisplayName(BlessedCorrectionDisplayName)
+                .SetDescription(BlessedCorrectionDescription)
+                .SetIcon(icon)
+                .SetBuff(BlessedCorrectionBuffGuid)
+                .SetDeactivateImmediately(true)
+                .AddActivatableAbilityResourceLogic(requiredResource: abilityresourse, spendType: ActivatableAbilityResourceLogic.ResourceSpendType.NewRound)
+                .Configure();
+
+            BuffConfigurator.New(BlessedCorrectionBuff, BlessedCorrectionBuffGuid)
+             .SetDisplayName(BlessedCorrectionDisplayName)
+             .SetDescription(BlessedCorrectionDescription)
+             .SetIcon(icon)
+             .AddModifyD20(replace: false, rollsAmount: 1, takeBest: true, rule: RuleType.SkillCheck, specificSkill: true, skill: new StatType[] { StatType.CheckIntimidate, StatType.CheckDiplomacy })
+             .AddToFlags(BlueprintBuff.Flags.HiddenInUi)
+             .AddToFlags(BlueprintBuff.Flags.StayOnDeath)
+             .Configure();
+
+            return FeatureConfigurator.New(BlessedCorrection, BlessedCorrectionGuid)
+              .SetDisplayName(BlessedCorrectionDisplayName)
+              .SetDescription(BlessedCorrectionDescription)
+              .SetIcon(icon)
+              .AddAbilityResources(resource: abilityresourse, restoreAmount: true)
+              .AddFacts(new() { ability })
+              .Configure();
+        }
+
+        private const string InspiredRhetoric = "Inquisition.InspiredRhetoric";
+        private static readonly string InspiredRhetoricGuid = "{CC6324D1-47BB-4465-81FD-AE5EA293B9A8}";
+
+        internal const string InquisitionInspiredRhetoricDisplayName = "InquisitionInspiredRhetoric.Name";
+        private const string InquisitionInspiredRhetoricDescription = "InquisitionInspiredRhetoric.Description";
+        public static BlueprintFeature InspiredRhetoricFeat()
+        {
+            var icon = FeatureRefs.Persuasive.Reference.Get().Icon;
+
+            return FeatureConfigurator.New(InspiredRhetoric, InspiredRhetoricGuid)
+              .SetDisplayName(InquisitionInspiredRhetoricDisplayName)
+              .SetDescription(InquisitionInspiredRhetoricDescription)
+              .SetIcon(icon)
+              .AddReplaceStatBaseAttribute(StatType.Wisdom, true, Kingmaker.UnitLogic.Buffs.BonusMod.AsIs, StatType.CheckDiplomacy)
+              .AddReplaceStatBaseAttribute(StatType.Wisdom, true, Kingmaker.UnitLogic.Buffs.BonusMod.AsIs, StatType.CheckIntimidate)
               .Configure();
         }
     }
