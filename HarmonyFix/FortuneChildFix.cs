29d868a64e177672579be656882ca8ee85cf6ddb
YLMstring <61271096+YLMstring@users.noreply.github.com>
2024-01-28 20:57:20 +0800

    Update FortuneChildFix.cs

--- HarmonyFix/FortuneChildFix.cs
+++ HarmonyFix/FortuneChildFix.cs
@@ -4,6 +4,7 @@ using HarmonyLib;
 using Kingmaker.Blueprints;
 using Kingmaker.Enums;
 using Kingmaker.RuleSystem;
+using Kingmaker.RuleSystem.Rules;
 using Kingmaker.UnitLogic;
 using Kingmaker.UnitLogic.Abilities;
 using PrestigePlus.Blueprint.Feat;
@@ -16,22 +17,23 @@ using static Kingmaker.Blueprints.Area.FactHolder;
 
 namespace PrestigePlus.HarmonyFix
 {
-    [HarmonyPatch(typeof(RulebookEvent), nameof(RulebookEvent.TotalBonusValue), MethodType.Getter)]
+    [HarmonyPatch(typeof(RulebookEvent))]
+    [HarmonyPatch("AddModifier")]
+    [HarmonyPatch(new Type[] { typeof(Modifier) })]
     internal class FortuneChildFix
     {
-        static void Postfix(ref RulebookEvent __instance, ref int __result)
+        private static readonly LogWrapper Logger = LogWrapper.Get("PrestigePlus");
+        static bool Prefix(ref RulebookEvent __instance, ref Modifier bonus)
         {
-            if (__instance.Initiator.HasFact(Ace))
+            Logger.Info(bonus.ToString() + " child");
+            var fact = __instance.Initiator.GetFact(Ace);
+            if (fact != null && bonus.Descriptor == ModifierDescriptor.Luck && bonus.Fact != fact)
             {
-                foreach (var mod in __instance.AllBonuses)
-                {
-                    if (mod.Descriptor == ModifierDescriptor.Luck)
-                    {
-                        __result += 1;
-                        return;
-                    }
-                }
+                Logger.Info(bonus.Value.ToString() + " child");
+                __instance.AddModifier(bonus.Value + 1, bonus.Fact, ModifierDescriptor.Luck);
+                return false;
             }
+            return true;
         }
         private static BlueprintFeatureReference Ace = BlueprintTool.GetRef<BlueprintFeatureReference>(DeificObedience.Chaldira2Guid);
     }
