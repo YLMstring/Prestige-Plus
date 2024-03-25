5652d0aea759da6b1dab99455729d594fac4d185
YLMstring <61271096+YLMstring@users.noreply.github.com>
2024-03-25 11:13:40 +0800

    virtuoso

---
+++ Blueprint/Spell/VirtuosoPerformance.cs
@@ -0,0 +1,74 @@
+﻿using BlueprintCore.Actions.Builder;
+using BlueprintCore.Blueprints.CustomConfigurators.UnitLogic.Abilities;
+using BlueprintCore.Blueprints.CustomConfigurators.UnitLogic.Buffs;
+using BlueprintCore.Blueprints.References;
+using BlueprintCore.Utils.Types;
+using Kingmaker.Blueprints.Classes.Spells;
+using Kingmaker.Craft;
+using Kingmaker.UnitLogic.Abilities.Blueprints;
+using Kingmaker.UnitLogic.Abilities;
+using PrestigePlus.CustomComponent.Spell;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Kingmaker.Visual.Animation.Kingmaker.Actions.UnitAnimationActionCastSpell;
+using BlueprintCore.Actions.Builder.ContextEx;
+using Kingmaker.Blueprints;
+using Kingmaker.UnitLogic.Abilities.Components.Base;
+using PrestigePlus.CustomAction.ClassRelated;
+
+namespace PrestigePlus.Blueprint.Spell
+{
+    internal class VirtuosoPerformance
+    {
+        private const string VirtuosoPerformanceAbility = "NewSpell.UseVirtuosoPerformance";
+        public static readonly string VirtuosoPerformanceAbilityGuid = "{31E5B2F7-B4AC-4B8C-BE0F-B9824EDE6FA7}";
+
+        private const string VirtuosoPerformanceBuff = "NewSpell.VirtuosoPerformanceBuff";
+        private static readonly string VirtuosoPerformanceBuffGuid = "{FA73F1B7-79E4-423D-919D-91665E1D4BED}";
+
+        internal const string DisplayName = "NewSpellVirtuosoPerformance.Name";
+        private const string Description = "NewSpellVirtuosoPerformance.Description";
+        public static void Configure()
+        {
+            var icon = FeatureRefs.FascinateFeature.Reference.Get().Icon;
+            var fx = AbilityRefs.Serenity.Reference.Get().GetComponent<AbilitySpawnFx>();
+
+            var action = ActionsBuilder.New()
+                .Add<VirtuosoSpend>(c => { c.ResGuid = AbilityResourceRefs.BardicPerformanceResource.ToString(); })
+                .Build();
+
+            var buff = BuffConfigurator.New(VirtuosoPerformanceBuff, VirtuosoPerformanceBuffGuid)
+              .SetDisplayName(DisplayName)
+              .SetDescription(Description)
+              .SetIcon(icon)
+              .AddNewRoundTrigger(newRoundActions: action)
+              .AddIncreaseActivatableAbilityGroupSize(Kingmaker.UnitLogic.ActivatableAbilities.ActivatableAbilityGroup.BardicPerformance)
+              .Configure();
+
+            AbilityConfigurator.NewSpell(
+                VirtuosoPerformanceAbility, VirtuosoPerformanceAbilityGuid, SpellSchool.Transmutation, canSpecialize: false)
+              .SetDisplayName(DisplayName)
+              .SetDescription(Description)
+              .SetIcon(icon)
+              .SetLocalizedDuration(AbilityRefs.Haste.Reference.Get().LocalizedDuration)
+              .SetAnimation(CastAnimationStyle.Kineticist)
+              .AddComponent(fx)
+              .SetRange(AbilityRange.Personal)
+              .SetType(AbilityType.Spell)
+              .SetAvailableMetamagic(Metamagic.CompletelyNormal, Metamagic.Heighten, Metamagic.Extend)
+              .AddToSpellLists(level: 1, SpellList.Bard)
+              .AddAbilityEffectRunAction(
+                actions: ActionsBuilder.New()
+                  .ApplyBuff(buff, ContextDuration.Variable(ContextValues.Rank()), isFromSpell: true)
+                  .Build())
+              .AddCraftInfoComponent(
+                aOEType: CraftAOE.None,
+                savingThrow: CraftSavingThrow.None,
+                spellType: CraftSpellType.Buff)
+              .Configure();
+        }
+    }
+}
---
+++ CustomAction/ClassRelated/VirtuosoSpend.cs
@@ -0,0 +1,42 @@
+﻿using BlueprintCore.Utils;
+using Kingmaker.Blueprints;
+using Kingmaker.UnitLogic.ActivatableAbilities;
+using Kingmaker.UnitLogic.Mechanics.Actions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrestigePlus.CustomAction.ClassRelated
+{
+    internal class VirtuosoSpend : ContextAction
+    {
+        public override string GetCaption()
+        {
+            return "VirtuosoSpend";
+        }
+
+        public override void RunAction()
+        {
+            if (Context.MaybeCaster == null) { return; }
+            int num = 0;
+            foreach (ActivatableAbility activatableAbility in Context.MaybeCaster.ActivatableAbilities.RawFacts)
+            {
+                if (activatableAbility.Blueprint.Group == ActivatableAbilityGroup.BardicPerformance && (activatableAbility.IsStarted || activatableAbility.IsOn))
+                {
+                    num += activatableAbility.Blueprint.WeightInGroup;
+                }
+            }
+            if (num < 2) { return; }
+            var Res = BlueprintTool.GetRef<BlueprintAbilityResourceReference>(ResGuid);
+            if (Res?.Get() != null && Context.MaybeCaster.Descriptor.Resources.GetResource(Res) != null)
+            {
+                Context.MaybeCaster.Descriptor.Resources.Restore(Res, 1);
+            }
+        }
+
+        public string ResGuid;
+    }
+}
