d250aa961977aaf9402d05cab93ff978a452e32d
YLMstring <61271096+YLMstring@users.noreply.github.com>
2024-04-12 09:45:00 +0800

    1

--- CustomComponent/PrestigeClass/PhaseArrow.cs
+++ CustomComponent/PrestigeClass/PhaseArrow.cs
@@ -4,6 +4,7 @@ using Kingmaker.Designers.Mechanics.Facts;
 using Kingmaker.Enums;
 using Kingmaker.PubSubSystem;
 using Kingmaker.RuleSystem.Rules;
+using Kingmaker.UnitLogic;
 using Kingmaker.UnitLogic.Abilities.Components;
 using System;
 using System.Collections.Generic;
@@ -16,20 +17,21 @@ using static UnityModManagerNet.UnityModManager.Param;
 namespace PrestigePlus.Modify
 {
     [TypeId("{29896BE9-4421-4B70-9A39-461444E1741C}")]
-    internal class PhaseArrow : BrilliantEnergy, IInitiatorRulebookHandler<RuleCalculateAC>
+    internal class PhaseArrow : UnitFactComponentDelegate, IInitiatorRulebookHandler<RuleCalculateAC>, IRulebookHandler<RuleCalculateAC>, ISubscriber, IInitiatorRulebookSubscriber
     {
         private static readonly LogWrapper Logger = LogWrapper.Get("PrestigePlus");
         void IRulebookHandler<RuleCalculateAC>.OnEventAboutToTrigger(RuleCalculateAC evt)
         {
             try
             {
-                if (evt.BrilliantEnergy == null)
-                {
-                    evt.AddModifier(-RuleCalculateAC.CalculateArmorAndShieldBonuses(evt.Target), evt.BrilliantEnergy, ModifierDescriptor.UntypedStackable);
-                }
-                base.OnEventAboutToTrigger(evt);
+                evt.AddModifier(-RuleCalculateAC.CalculateArmorAndShieldBonuses(evt.Target), Fact, ModifierDescriptor.Penalty);
             }
             catch (Exception e) { Logger.Error("Failed to phase arrow.", e); }
+        }
+
+        void IRulebookHandler<RuleCalculateAC>.OnEventDidTrigger(RuleCalculateAC evt)
+        {
+            
         }
     }
 }
