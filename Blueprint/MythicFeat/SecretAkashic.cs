c696278d2f7c0750c9b91e2a93c8a0b915f7fff9
YLMstring <61271096+YLMstring@users.noreply.github.com>
2024-04-16 15:37:16 +0800

    2

---
+++ Blueprint/MythicFeat/SecretAkashic.cs
@@ -0,0 +1,47 @@
+﻿using BlueprintCore.Blueprints.CustomConfigurators.Classes;
+using BlueprintCore.Blueprints.References;
+using BlueprintCore.Utils;
+using Kingmaker.Blueprints;
+using PrestigePlus.Blueprint.Spell;
+using PrestigePlus.CustomComponent.Spell;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrestigePlus.Blueprint.MythicFeat
+{
+    internal class SecretAkashic
+    {
+        private static readonly string FeatName = "SecretAkashic";
+        private static readonly string FeatGuid = "{9B8CA340-96B2-4465-BC66-595279EB8382}";
+
+        private static readonly string DisplayName = "MythicSecretAkashic.Name";
+        private static readonly string Description = "MythicSecretAkashic.Description";
+
+        public static void Configure()
+        {
+            var icon = AbilityRefs.TrueSeeing.Reference.Get().Icon;
+
+            FeatureConfigurator.New(FeatName, FeatGuid, Kingmaker.Blueprints.Classes.FeatureGroup.MythicFeat)
+                    .SetDisplayName(DisplayName)
+                    .SetDescription(Description)
+                    .SetIcon(icon)
+                    .AddPrerequisiteFeature(ProgressionRefs.KineticBlastProgression.ToString())
+                    .AddPrerequisiteFeature(FeatureRefs.LoremasterGreaterLore.ToString())
+                    .AddToFeatureSelection("0d3a3619-9d99-47af-8e47-cb6cc4d26821") //ttt
+                    .AddComponent<PPLearnSpell>(c =>
+                    {
+                        c.Spell = BlueprintTool.GetRef<BlueprintAbilityReference>(GlimpseAkashic.GlimpseAkashicAbilityGuid);
+                        c.level = 8;
+                    })
+                    .AddComponent<PPLearnSpell>(c =>
+                    {
+                        c.Spell = BlueprintTool.GetRef<BlueprintAbilityReference>(AkashicForm.AkashicFormAbilityGuid);
+                        c.level = 9;
+                    })
+                    .Configure();
+        }
+    }
+}
---
+++ CustomComponent/Spell/PPLearnSpell.cs
@@ -0,0 +1,61 @@
+﻿using Kingmaker.Blueprints.Classes.Spells;
+using Kingmaker.Blueprints.Classes;
+using Kingmaker.Blueprints;
+using Kingmaker.EntitySystem.Entities;
+using Kingmaker.PubSubSystem;
+using Kingmaker.UnitLogic.Abilities.Blueprints;
+using Kingmaker.UnitLogic.Abilities;
+using Kingmaker.UnitLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine.Serialization;
+using UnityEngine;
+
+namespace PrestigePlus.CustomComponent.Spell
+{
+    internal class PPLearnSpell : UnitFactComponentDelegate, ILevelUpCompleteUIHandler, IGlobalSubscriber, ISubscriber
+    {
+        public BlueprintCharacterClass CharacterClass;
+        public BlueprintAbility Spell;
+        public int level;
+        public override void OnTurnOn()
+        {
+            this.Learn();
+        }
+        public void HandleLevelUpComplete(UnitEntityData unit, bool isChargen)
+        {
+            this.Learn();
+        }
+
+        private void Learn()
+        {
+            if (this.CharacterClass != null)
+            {
+                LearnList(this.CharacterClass);
+            }
+            else
+            {
+                foreach (var data in Owner.Progression.Classes)
+                {
+                    LearnList(data.CharacterClass);
+                }
+            }
+        }
+        private void LearnList(BlueprintCharacterClass clazz)
+        {
+            ClassData classData = base.Owner.Progression.GetClassData(clazz);
+            if (classData == null)
+            {
+                return;
+            }
+            Spellbook spellbook = base.Owner.DemandSpellbook(clazz);
+            if (spellbook.GetKnownSpells(level).All((AbilityData spellFromSpellbook) => spellFromSpellbook.Blueprint != Spell))
+            {
+                spellbook.AddKnown(level, Spell, true);
+            }
+        }
+    }
+}
