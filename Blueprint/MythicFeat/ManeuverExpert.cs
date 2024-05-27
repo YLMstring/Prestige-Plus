803bf48aab5ac82849f6f3759a06704b05a5cfb8
YLMstring <61271096+YLMstring@users.noreply.github.com>
2024-05-27 10:26:37 +0800

    1

--- Blueprint/MythicFeat/ManeuverExpert.cs
+++ Blueprint/MythicFeat/ManeuverExpert.cs
@@ -27,7 +27,8 @@ namespace PrestigePlus.Blueprint.MythicFeat
                     .SetDisplayName(DisplayName)
                     .SetDescription(Description)
                     .SetIcon(icon)
-                    .AddBuffAllSavesBonus(Kingmaker.Enums.ModifierDescriptor.Penalty, -2)
+                    //.AddBuffAllSavesBonus(Kingmaker.Enums.ModifierDescriptor.Penalty, -2)
+                    .AddStatBonus(Kingmaker.Enums.ModifierDescriptor.Penalty, false, Kingmaker.EntitySystem.Stats.StatType.AdditionalAttackBonus, -2)
                     .AddFacts(new() { FeatureRefs.ImprovedBullRush.ToString(), FeatureRefs.ImprovedDirtyTrick.ToString(), FeatureRefs.ImprovedDisarm.ToString(), FeatureRefs.ImprovedSunder.ToString(), FeatureRefs.ImprovedTrip.ToString(),
                     FeatureRefs.GreaterBullRush.ToString(), FeatureRefs.GreaterDirtyTrick.ToString(), FeatureRefs.GreaterDisarm.ToString(), FeatureRefs.GreaterSunder.ToString(), FeatureRefs.GreaterTrip.ToString(), ImprovedGrapple.StyleGuid, GreaterGrapple.FeatGuid,
                     FeatureRefs.ImprovedOverrun.ToString(), AbilityRefs.OverrunAbility.ToString() })
