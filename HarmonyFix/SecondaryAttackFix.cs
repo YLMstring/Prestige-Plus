b193237c41c629b9adff80f43fca99411b171186
YLMstring <61271096+YLMstring@users.noreply.github.com>
2024-06-11 16:09:47 +0800

    1

---
+++ HarmonyFix/SecondaryAttackFix.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using Kingmaker.RuleSystem.Rules;
+using Kingmaker.UnitLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrestigePlus.HarmonyFix
+{
+    [HarmonyPatch(typeof(RuleAttackWithWeapon), nameof(RuleAttackWithWeapon.WeaponStats), MethodType.Getter)]
+    internal class SecondaryAttackFix
+    {
+        static void Postfix(RuleAttackWithWeapon __instance, ref RuleCalculateWeaponStats __result)
+        {
+            if (__instance.IsFullAttack == false)
+            {
+                __result.IsSecondaryOverride = false;
+                __result.IsSecondary = false;
+            }
+        }
+    }
+}
