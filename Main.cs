b60e78f5892c7c33e2e552db3660b0487aa2de9b
YLMstring <61271096+YLMstring@users.noreply.github.com>
2024-07-01 18:57:44 +0800

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -411,7 +411,7 @@ namespace PrestigePlus
         {
           if (Initialized)
           {
-            Logger.Info("Already configured blueprints.");
+            Logger.Info("Already configured blueprints");
             return;
           }
           Initialized = true;
