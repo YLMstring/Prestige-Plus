9cbb151ae5789aef0ea0f747e9d1d159ae761c5b
YLMstring <61271096+YLMstring@users.noreply.github.com>
2024-07-05 16:32:28 +0800

    1

---
+++ Blueprint/Spell/EmblemGreed.cs
@@ -0,0 +1,188 @@
+﻿using BlueprintCore.Actions.Builder;
+using BlueprintCore.Blueprints.CustomConfigurators.UnitLogic.Abilities;
+using BlueprintCore.Blueprints.CustomConfigurators.UnitLogic.Buffs;
+using BlueprintCore.Blueprints.References;
+using BlueprintCore.Utils.Types;
+using Kingmaker.Blueprints.Classes.Spells;
+using Kingmaker.UnitLogic.Abilities;
+using Kingmaker.UnitLogic.Buffs;
+using Kingmaker.UnitLogic.Parts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kingmaker.Craft;
+using Kingmaker.UnitLogic.Abilities.Blueprints;
+using BlueprintCore.Actions.Builder.ContextEx;
+using static Kingmaker.Visual.Animation.Kingmaker.Actions.UnitAnimationActionCastSpell;
+using BlueprintCore.Blueprints.Configurators.Items.Weapons;
+using BlueprintCore.Blueprints.Configurators.Items.Ecnchantments;
+using Kingmaker.Blueprints.Items.Weapons;
+using Kingmaker.Blueprints;
+using Kingmaker.ElementsSystem;
+using Kingmaker.Items.Slots;
+using Kingmaker.Items;
+using Kingmaker.PubSubSystem;
+using Kingmaker.UnitLogic.Buffs.Components;
+using Kingmaker.UnitLogic.Class.Kineticist;
+using Kingmaker.UnitLogic;
+using Kingmaker;
+using Owlcat.QA.Validation;
+using UnityEngine.Serialization;
+using UnityEngine;
+
+namespace PrestigePlus.Blueprint.Spell
+{
+    internal class EmblemGreed
+    {
+        private const string EmblemGreedAbility = "NewSpell.UseEmblemGreed";
+        public static readonly string EmblemGreedAbilityGuid = "{B7D3605D-22A7-4D31-B6FF-52C3BDE43CCA}";
+
+        private const string EmblemGreedBuff = "NewSpell.EmblemGreedBuff";
+        public static readonly string EmblemGreedBuffGuid = "{1034AF48-D063-43E1-935C-959D39484484}";
+
+        internal const string DisplayName = "NewSpellEmblemGreed.Name";
+        private const string Description = "NewSpellEmblemGreed.Description";
+
+        private const string MaximName = "EmblemGreedWeapon";
+        public static readonly string MaximGuid = "{3E39268B-E1E6-404B-8021-E5C03D613C6B}";
+        public static void Configure()
+        {
+            var icon = AbilityRefs.ArmyGreedySands.Reference.Get().Icon;
+
+            var glaive = ItemWeaponRefs.GlaiveFlamingPlus1.Reference.Get();
+
+            var maxim = ItemWeaponConfigurator.New(MaximName, MaximGuid)
+                .SetDisplayNameText(DisplayName)
+                .SetDescriptionText(Description)
+                .SetFlavorText(Description)
+                .SetIcon(glaive.Icon)
+                .SetVisualParameters(glaive.m_VisualParameters)
+                .SetType(WeaponTypeRefs.Glaive.ToString())
+                .AddToEnchantments(WeaponEnchantmentRefs.Flaming.ToString())
+                .Configure();
+
+            var buff = BuffConfigurator.New(EmblemGreedBuff, EmblemGreedBuffGuid)
+              .SetDisplayName(DisplayName)
+              .SetDescription(Description)
+              .SetIcon(icon)
+              .AddComponent<AddGreedBlade>(c => { c.Blade = maxim; })
+              .Configure();
+
+            AbilityConfigurator.NewSpell(EmblemGreedAbility, EmblemGreedAbilityGuid, SpellSchool.Transmutation, canSpecialize: true)
+                .SetDisplayName(DisplayName)
+                .SetDescription(Description)
+              .SetIcon(icon)
+              .SetAnimation(CastAnimationStyle.SelfTouch)
+              .SetRange(AbilityRange.Personal)
+              .SetLocalizedDuration(AbilityRefs.EnlargePerson.Reference.Get().LocalizedDuration)
+              .SetAvailableMetamagic(Metamagic.CompletelyNormal, Metamagic.Heighten, Metamagic.Extend, Metamagic.Quicken)
+              .AddToSpellLists(level: 6, SpellList.Cleric)
+              .AddToSpellLists(level: 6, SpellList.Inquisitor)
+              .AddToSpellLists(level: 7, SpellList.Shaman)
+              .AddToSpellLists(level: 6, SpellList.Wizard)
+              .AddToSpellLists(level: 6, SpellList.Magus)
+              .AddAbilityEffectRunAction(
+                actions: ActionsBuilder.New()
+                  .ApplyBuff(buff, ContextDuration.Variable(ContextValues.Rank(), Kingmaker.UnitLogic.Mechanics.DurationRate.Minutes), isFromSpell: true)
+                  .Build())
+              .AddSpellDescriptorComponent(SpellDescriptor.Polymorph)
+              .Configure();
+        }
+    }
+
+    public class AddGreedBlade : UnitBuffComponentDelegate<AddKineticistBladeData>, IAreaActivationHandler, IGlobalSubscriber, ISubscriber
+    {
+        public BlueprintItemWeapon Blade;
+
+        // Token: 0x0600CC04 RID: 52228 RVA: 0x00350D10 File Offset: 0x0034EF10
+        public override void OnActivate()
+        {
+            base.OnActivate();
+            base.Owner.MarkNotOptimizableInSave(true);
+            base.Data.Applied = this.Blade.CreateEntity<ItemEntityWeapon>();
+            base.Data.Applied.MakeNotLootable();
+            HandSlot handSlot = Owner.Body.PrimaryHand;
+            if (!handSlot.CanInsertItem(base.Data.Applied, false))
+            {
+                base.Data.Applied = null;
+                PFLog.Default.Error("Can't insert kineticist weapon to target hand", Array.Empty<object>());
+                return;
+            }
+            using (ContextData<ItemsCollection.SuppressEvents>.Request())
+            {
+                handSlot.InsertItem(base.Data.Applied);
+            }
+        }
+
+        // Token: 0x0600CC05 RID: 52229 RVA: 0x00350E00 File Offset: 0x0034F000
+        public override void OnDeactivate()
+        {
+            base.OnDeactivate();
+            if (base.Data.Applied != null)
+            {
+                ItemSlot holdingSlot = base.Data.Applied.HoldingSlot;
+                if (holdingSlot != null)
+                {
+                    holdingSlot.RemoveItem(true);
+                }
+                using (ContextData<ItemsCollection.SuppressEvents>.Request())
+                {
+                    ItemsCollection collection = base.Data.Applied.Collection;
+                    if (collection != null)
+                    {
+                        collection.Remove(base.Data.Applied);
+                    }
+                }
+                base.Data.Applied = null;
+            }
+        }
+        public override void ApplyValidation(ValidationContext context, int parentIndex)
+        {
+            base.ApplyValidation(context, parentIndex);
+        }
+
+        // Token: 0x0600CC07 RID: 52231 RVA: 0x00350ECF File Offset: 0x0034F0CF
+        public override void OnTurnOn()
+        {
+            ItemEntityWeapon applied = base.Data.Applied;
+            if (applied == null)
+            {
+                return;
+            }
+            ItemSlot holdingSlot = applied.HoldingSlot;
+            if (holdingSlot == null)
+            {
+                return;
+            }
+            holdingSlot.Lock.Retain();
+        }
+
+        // Token: 0x0600CC08 RID: 52232 RVA: 0x00350EF5 File Offset: 0x0034F0F5
+        public override void OnTurnOff()
+        {
+            ItemEntityWeapon applied = base.Data.Applied;
+            if (applied == null)
+            {
+                return;
+            }
+            ItemSlot holdingSlot = applied.HoldingSlot;
+            if (holdingSlot == null)
+            {
+                return;
+            }
+            holdingSlot.Lock.Release();
+        }
+
+        // Token: 0x0600CC09 RID: 52233 RVA: 0x00350F1B File Offset: 0x0034F11B
+        public void OnAreaActivated()
+        {
+            if (base.Data.Applied == null)
+            {
+                this.OnActivate();
+                this.OnTurnOn();
+            }
+        }
+    }
+}
--- HarmonyFix/ThrowPunchAttackCount.cs
+++ HarmonyFix/ThrowPunchAttackCount.cs
@@ -5,8 +5,10 @@ using Kingmaker.RuleSystem.Rules;
 using Kingmaker.RuleSystem.Rules.Damage;
 using Kingmaker.Settings;
 using Kingmaker.UnitLogic;
+using Kingmaker.UnitLogic.Buffs;
 using Kingmaker.UnitLogic.Parts;
 using PrestigePlus.Blueprint.Feat;
+using PrestigePlus.Blueprint.Spell;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,7 +26,7 @@ namespace PrestigePlus.HarmonyFix
             try
             {
                 var caster = __instance.Initiator;
-                if (caster.HasFact(Buff) && caster.GetThreatHandMelee()?.Weapon?.Blueprint.Category == Kingmaker.Enums.WeaponCategory.UnarmedStrike)
+                if (caster.HasFact(Buff1) && caster.Body.PrimaryHand?.Weapon?.Blueprint.Category == Kingmaker.Enums.WeaponCategory.UnarmedStrike)
                 {
                     int num = 0;
                     foreach (ClassData classData2 in caster.Descriptor.Progression.Classes)
@@ -42,12 +44,52 @@ namespace PrestigePlus.HarmonyFix
                     __result = num2;
                     return false;
                 }
+                if (caster.GetFact(Buff2) is Buff buff && caster.Body.PrimaryHand?.Weapon?.Blueprint.AssetGuidThreadSafe == EmblemGreed.MaximGuid)
+                {
+                    int num = buff.Context.Params.CasterLevel;
+                    int num2 = Math.Max(0, num / 5 - ((num % 5 == 0) ? 1 : 0));
+                    if (num2 > 3 && caster.Get<UnitPartCompanion>() == null)
+                    {
+                        num2 = 3;
+                    }
+                    __result = num2;
+                    return false;
+                }
                 return true;
             }
-            catch (Exception ex) { Logger.Error("Failed to ThrowPunchAttackCount.", ex); return true; }
+            catch (Exception ex) { Main.Logger.Error("Failed to ThrowPunchAttackCount.", ex); return true; }
         }
-        private static readonly LogWrapper Logger = LogWrapper.Get("PrestigePlus");
-        private static BlueprintBuffReference Buff = BlueprintTool.GetRef<BlueprintBuffReference>(MageHandTrick.ThrowPunchbuffGuid);
-        //private static BlueprintBuffReference Buff2 = BlueprintTool.GetRef<BlueprintBuffReference>(MageHandTrick.MageHandMythicFeatGuid);
+        private static BlueprintBuffReference Buff1 = BlueprintTool.GetRef<BlueprintBuffReference>(MageHandTrick.ThrowPunchbuffGuid);
+        private static BlueprintBuffReference Buff2 = BlueprintTool.GetRef<BlueprintBuffReference>(EmblemGreed.EmblemGreedBuffGuid);
+    }
+
+    [HarmonyPatch(typeof(RuleCalculateWeaponStats), nameof(RuleCalculateWeaponStats.OnTrigger))]
+    internal class ThrowPunchAttackCount2
+    {
+        static void Prefix(ref RuleCalculateWeaponStats __instance)
+        {
+            try
+            {
+                var caster = __instance.Initiator;
+                if (caster.GetFact(Buff2) is Buff buff && __instance.Weapon.Blueprint.AssetGuidThreadSafe == EmblemGreed.MaximGuid)
+                {
+                    int num = buff.Context.Params.CasterLevel;
+                    if (num >= 19)
+                    {
+                        __instance.EnhancementTotal = 3;
+                    }
+                    else if (num >= 15)
+                    {
+                        __instance.EnhancementTotal = 2;
+                    }
+                    else
+                    {
+                        __instance.EnhancementTotal = 1;
+                    }
+                }
+            }
+            catch (Exception ex) { Main.Logger.Error("Failed to ThrowPunchAttackCount2.", ex); }
+        }
+        private static BlueprintBuffReference Buff2 = BlueprintTool.GetRef<BlueprintBuffReference>(EmblemGreed.EmblemGreedBuffGuid);
     }
 }
