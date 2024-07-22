8a49aff4a3be94b0f8eb7fc9d925c612fa6d6e07
YLMstring <61271096+YLMstring@users.noreply.github.com>
2024-07-22 11:02:48 +0800

    1

--- Blueprint/AutoHeal.cs
+++ Blueprint/AutoHeal.cs
@@ -76,7 +76,7 @@ namespace PrestigePlus.Blueprint
 
             FeatureConfigurator.For(FeatureRefs.SkillAbilities)
                     .AddFacts(new() { ability, ability2 })
-                    //.AddIncreaseResourceAmount("AC63BFCF-EC31-43DC-A5CE-04617A3BC854", 3)
+                    .AddCombatStateTrigger(combatStartActions: ActionsBuilder.New().Add<GatherSummons>().Build())
                     .Configure();
         }
 
