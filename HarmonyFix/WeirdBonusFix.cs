40d7eb6c823267b40acf45e564aa84cf6f21780d
YLMstring <61271096+YLMstring@users.noreply.github.com>
2024-08-09 17:34:25 +0800

    Update WeirdBonusFix.cs

--- HarmonyFix/WeirdBonusFix.cs
+++ HarmonyFix/WeirdBonusFix.cs
@@ -1,10 +1,12 @@
 ﻿using BlueprintCore.Blueprints.References;
 using HarmonyLib;
+using Kingmaker.Blueprints;
 using Kingmaker.Designers.Mechanics.Facts;
 using Kingmaker.EntitySystem.Entities;
 using Kingmaker.PubSubSystem;
 using Kingmaker.RuleSystem;
 using Kingmaker.RuleSystem.Rules;
+using Kingmaker.UI.MVVM._VM.Slots;
 using Kingmaker.UnitLogic;
 using Kingmaker.UnitLogic.Abilities.Blueprints;
 using Kingmaker.UnitLogic.FactLogic;
@@ -12,14 +14,36 @@ using Kingmaker.UnitLogic.Mechanics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Kingmaker.Utility;
 
 namespace PrestigePlus.HarmonyFix
 {
-    [HarmonyPatch(typeof(ModifyD20), nameof(IRulebookHandler<RuleCalculateAttackBonus>.OnEventAboutToTrigger))]
+    [HarmonyPatch(typeof(ModifyD20))]
+    //[HarmonyPatch("OnEventAboutToTrigger")]
+    //[HarmonyPatch([typeof(RuleCalculateAttackBonus)])]
     internal class WeirdBonusFix
     {
+        [HarmonyTargetMethod]
+        static MethodInfo TargetMethod()
+        {
+            var interfaceMap = typeof(ModifyD20).GetInterfaceMap(typeof(IRulebookHandler<RuleCalculateAttackBonus>));
+
+            if (interfaceMap.InterfaceMethods
+                .Zip(interfaceMap.TargetMethods)
+                .TryFind((pair) =>
+                    pair.Item1.Name == nameof(IRulebookHandler<RuleCalculateAttackBonus>.OnEventAboutToTrigger),
+                    out var pair))
+            {
+                return pair.Item2;
+            }
+
+            throw new Exception("Missing interface method");
+        }
+
+        [HarmonyPrefix]
         static bool Prefix()
         {
             RulebookEvent previousEvent = Rulebook.CurrentContext.PreviousEvent;
