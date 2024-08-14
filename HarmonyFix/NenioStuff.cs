51db3af47ce3a21429106a1297988f4af120dbd9
YLMstring <61271096+YLMstring@users.noreply.github.com>
2024-08-14 21:35:40 +0800

    Update NenioStuff.cs

--- HarmonyFix/NenioStuff.cs
+++ HarmonyFix/NenioStuff.cs
@@ -60,26 +60,4 @@ namespace PrestigePlus.HarmonyFix
             catch { }
         }
     }
-
-    [HarmonyPatch(typeof(LocalizedString), nameof(LocalizedString.LoadString))]
-    internal class DualLanguageMode3
-    {
-        static void Postfix(ref string __result, ref LocalizedString __instance)
-        {
-            try
-            {
-                if (!__result.Contains(".") && !__result.Contains("。")) { return; }
-                string actualKey = __instance.GetActualKey();
-                LocalizationPack.StringEntry stringEntry;
-                if (!pack1.m_Strings.TryGetValue(actualKey, out stringEntry) || !pack2.m_Strings.TryGetValue(actualKey, out stringEntry))
-                {
-                    __result += " (mod)";
-                }
-            }
-            catch { }
-        }
-
-        private static LocalizationPack pack1 = LocalizationManager.LoadPack(Locale.enGB);
-        private static LocalizationPack pack2 = LocalizationManager.LoadPack(Locale.zhCN);
-    }
 }
