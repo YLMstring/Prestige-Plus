b78dbc88cb49638afc845f2307e9127c9037b766
YLMstring <61271096+YLMstring@users.noreply.github.com>
2024-08-17 19:47:39 +0800

    Revert "1"
    This reverts commit b5e8fd78a1f0189c68a9a29ae925eb65d36eaca8.

--- Blueprint/CombatStyle/BlackSeraphStyle.cs
+++ Blueprint/CombatStyle/BlackSeraphStyle.cs
@@ -154,15 +154,14 @@ namespace PrestigePlus.Feats
               .AddComponent<BuffDescriptorImmunityIgnore>(c => {
                   c.Descriptor = SpellDescriptor.Fear | SpellDescriptor.Shaken | SpellDescriptor.Frightened | SpellDescriptor.MindAffecting | SpellDescriptor.Emotion | SpellDescriptor.NegativeEmotion;
               })
-              .AddSavingThrowBonusAgainstDescriptor(1, modifierDescriptor: ModifierDescriptor.Resistance, spellDescriptor: SpellDescriptor.MindAffecting, value: 5)
               .Configure();
 
             var area = AbilityAreaEffectConfigurator.New(AnnihilationAura, AnnihilationAuraGuid)
-                .AddAbilityAreaEffectBuff(Buff2)
+                .AddAbilityAreaEffectBuff(Buff2, checkConditionEveryRound: true, condition: ConditionsBuilder.New().IsCaster(true).Build())
                 .SetAggroEnemies(true)
                 .SetAffectEnemies(true)
                 .SetAffectDead(true)
-                .SetTargetType(BlueprintAbilityAreaEffect.TargetType.Enemy)
+                .SetTargetType(BlueprintAbilityAreaEffect.TargetType.Any)
                 .SetShape(AreaEffectShape.Cylinder)
                 .SetSize(FeetExtension.Feet(33))
                 .Configure();
