b1b9ae2e36e714a05628baba2e0b9557095de052
YLMstring <61271096+YLMstring@users.noreply.github.com>
2024-08-20 19:25:29 +0800

    Create SkippableSelectionFix.cs

---
+++ HarmonyFix/SkippableSelectionFix.cs
@@ -0,0 +1,29 @@
+﻿using Kingmaker.UnitLogic.Abilities.Blueprints;
+using Kingmaker.UnitLogic.Abilities;
+using Kingmaker.UnitLogic.Commands.Base;
+using PrestigePlus.CustomComponent.Feat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using Kingmaker.UnitLogic.Commands;
+using Kingmaker.UI.MVVM._VM.CharGen.Phases.FeatureSelector;
+using Kingmaker.Blueprints.Classes.Selection;
+
+namespace PrestigePlus.HarmonyFix
+{
+    [HarmonyPatch(typeof(CharGenFeatureSelectorPhaseVM), nameof(CharGenFeatureSelectorPhaseVM.CheckIsCompleted))]
+    internal class SkippableSelectionFix
+    {
+        static void Postfix(ref bool __result, ref CharGenFeatureSelectorPhaseVM __instance)
+        {
+            if (__result) { return; }
+            if (__instance.FeatureSelectorStateVM?.Feature is BlueprintFeatureSelection feats && !feats.IsObligatory())
+            {
+                __result = true;
+            }
+        }
+    }
+}
