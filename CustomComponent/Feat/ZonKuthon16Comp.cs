0975d323397198669a9b0beb3460771531a3da96
YLMstring <61271096+YLMstring@users.noreply.github.com>
2024-09-06 12:18:15 +0800

    Create ZonKuthon16Comp.cs

---
+++ CustomComponent/Feat/ZonKuthon16Comp.cs
@@ -0,0 +1,48 @@
+﻿using BlueprintCore.Blueprints.References;
+using BlueprintCore.Utils;
+using Kingmaker.Blueprints;
+using Kingmaker.Enums;
+using Kingmaker.PubSubSystem;
+using Kingmaker.RuleSystem.Rules;
+using Kingmaker.UnitLogic;
+using Kingmaker.UnitLogic.Buffs.Blueprints;
+using Kingmaker.UnitLogic.Buffs.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Pathfinding.Util.RetainedGizmos;
+
+namespace PrestigePlus.CustomComponent.Feat
+{
+    internal class ZonKuthon16Comp : UnitBuffComponentDelegate, IInitiatorRulebookHandler<RuleAttackWithWeapon>, IRulebookHandler<RuleAttackWithWeapon>, ISubscriber, IInitiatorRulebookSubscriber
+    {
+        void IRulebookHandler<RuleAttackWithWeapon>.OnEventAboutToTrigger(RuleAttackWithWeapon evt)
+        {
+
+        }
+
+        void IRulebookHandler<RuleAttackWithWeapon>.OnEventDidTrigger(RuleAttackWithWeapon evt)
+        {
+            bool unarmed = false;
+            if (evt.Weapon?.Blueprint.Category == WeaponCategory.UnarmedStrike)
+            {
+                unarmed = true;
+            }
+            else if (Owner.HasFact(Feral) && evt.Weapon?.Blueprint.IsNatural == true)
+            {
+                unarmed = true;
+            }
+            if (!unarmed) { return; }
+            if (evt.AttackRoll?.IsHit == true) 
+            { 
+
+            }
+            Buff.Remove();
+        }
+
+        //FeralCombatTrainingFeature	edb2546d1215491ca404f8dd1a0c2af3	Kingmaker.Blueprints.Classes.BlueprintFeature
+        private static BlueprintFeatureReference Feral = BlueprintTool.GetRef<BlueprintFeatureReference>("edb2546d1215491ca404f8dd1a0c2af3");
+    }
+}
