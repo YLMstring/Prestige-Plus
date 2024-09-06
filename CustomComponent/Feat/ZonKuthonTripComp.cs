82ceaf8174cb6b8f341709ae0705935212705d7e
YLMstring <61271096+YLMstring@users.noreply.github.com>
2024-09-06 22:49:10 +0800

    1

--- CustomComponent/Feat/ZonKuthonTripComp.cs
+++ CustomComponent/Feat/ZonKuthonTripComp.cs
@@ -13,6 +13,7 @@ using Kingmaker.Enums;
 using Kingmaker.Items;
 using static Kingmaker.EntitySystem.EntityDataBase;
 using static Pathfinding.Util.RetainedGizmos;
+using static Kingmaker.Visual.Animation.Kingmaker.Actions.TimedProbabilityCurve;
 
 namespace PrestigePlus.CustomComponent.Feat
 {
@@ -26,17 +27,38 @@ namespace PrestigePlus.CustomComponent.Feat
         // Token: 0x0600EA11 RID: 59921 RVA: 0x003C2DD7 File Offset: 0x003C0FD7
         public void OnEventDidTrigger(RuleCombatManeuver evt)
         {
-            if (evt.Type == CombatManeuver.Trip && evt.Success)
+            if (evt.Type == CombatManeuver.Trip && evt.Success && Owner.CombatState.IsEngage(evt.Target))
             {
                 var wep = Owner.GetThreatHandMelee()?.Weapon;
                 if (wep?.Blueprint?.Category == WeaponCategory.Flail || wep?.Blueprint?.Category == WeaponCategory.HeavyFlail)
                 {
-                    if (Owner.CombatState.AttackOfOpportunity(evt.Target))
+                    if (Owner.CombatState.AttackOfOpportunity(evt.Target, false, true))
                     {
-                        Game.Instance.CombatEngagementController.ForceAttackOfOpportunity(Owner, evt.Target, false);
+                        EventBus.RaiseEvent<IAttackOfOpportunityHandler>(delegate (IAttackOfOpportunityHandler h)
+                        {
+                            h.HandleAttackOfOpportunity(Owner, evt.Target);
+                        }, true);
+                        Owner.CombatState.AttackOfOpportunityCount--;
+                        RunAttackRule(Owner, evt.Target, wep);
                     }
                 }
             }
+        }
+        private static void RunAttackRule(UnitEntityData maybeCaster, UnitEntityData unit, ItemEntityWeapon weapon)
+        {
+            RuleAttackWithWeapon ruleAttackWithWeapon = new(maybeCaster, unit, weapon, 0)
+            {
+                Reason = maybeCaster.Context,
+                AutoHit = false,
+                AutoCriticalThreat = false,
+                AutoCriticalConfirmation = false,
+                ExtraAttack = true,
+                IsFullAttack = false,
+                IsAttackOfOpportunity = true,
+                AttackNumber = 0,
+                AttacksCount = 1
+            };
+            maybeCaster.Context.TriggerRule(ruleAttackWithWeapon);
         }
     }
 }
