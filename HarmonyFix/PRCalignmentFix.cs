01e154ae4bf59c18dc84efda5693e7b8c5099312
YLMstring <61271096+YLMstring@users.noreply.github.com>
2024-09-20 14:13:06 +0800

    Create PRCalignmentFix.cs

---
+++ HarmonyFix/PRCalignmentFix.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using Kingmaker.Blueprints.Classes;
+using Kingmaker.Blueprints.Classes.Prerequisites;
+using Kingmaker.Blueprints.Items.Weapons;
+using Kingmaker.EntitySystem.Entities;
+using Kingmaker.Items;
+using Kingmaker.UnitLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrestigePlus.HarmonyFix
+{
+    [HarmonyPatch(typeof(PrerequisiteAlignment), nameof(PrerequisiteAlignment.CheckInternal))]
+    internal class PRCalignmentFix
+    {
+        static void Postfix(ref bool __result, ref UnitDescriptor unit, ref PrerequisiteAlignment __instance)
+        {
+            try
+            {
+                if (__result) { return; }
+                if (__instance.OwnerBlueprint is BlueprintCharacterClass clazz && clazz.PrestigeClass && unit.Progression.GetClassLevel(clazz) > 0)
+                {
+                    __result = true;
+                }
+            }
+            catch (Exception ex) { Main.Logger.Error("Fail to PRCalignmentFix.", ex); }
+        }
+    }
+}
